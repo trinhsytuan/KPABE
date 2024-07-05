73d42adff2a9af95776ebfdf64c337a99809acc9
Tuan <trinhsytuan@gmail.com>
2024-07-05 10:24:06 +0700

    Update Decrypt

--- Decrypt.cpp
+++ Decrypt.cpp
@@ -1,3 +1,11 @@
 //
 // Created by Trinh Tuan on 01/07/2024.
 //
+#include"Decrypt.h"
+
+void decrypt(pairing_t pairing, global_parameter params, element_t C1, element_t C2, keyUser keyUser) {
+    element_t Z1;
+    element_init_G2(Z1, pairing);
+
+
+}
--- Decrypt.h
+++ Decrypt.h
@@ -1,8 +1,10 @@
 //
 // Created by Trinh Tuan on 01/07/2024.
 //
-
+#include<pbc.h>
+#include"global_params.h"
+#include"KeyGenerator.h"
 #ifndef KPABE_DECRYPT_H
 #define KPABE_DECRYPT_H
-
-#endif //KPABE_DECRYPT_H
+void decrypt(pairing_t pairing, global_parameter params, element_t C1, element_t C2, keyUser keyUser);
+#endif //KPABE_DECRYPT_Hx
